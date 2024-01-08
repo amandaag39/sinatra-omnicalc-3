c116f59e1d2567273d244140a09f85e00f867273
Amanda Mendez <71214594+amandaag39@users.noreply.github.com>
2024-01-08 04:57:55 +0000

    Got a start on the app

--- app.rb
+++ app.rb
@@ -1,9 +1,18 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
 
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
+end
+get("/umbrella") do
+  erb(:umbrella_form)
+end
+
+get("/process_umbrella") do
+  @user_location = params.fetch("user_loc")
+  erb(:umbrella_results)
 end
